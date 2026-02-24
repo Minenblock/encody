bbaad8fe019bb0eb6acf031655649245d0616f87
Minenblock <robloxhd396@gmail.com>
2026-02-24 12:50:39 +0100

    template

---
+++ src/main/java/org/fascinate/encody/L_nico.java
@@ -0,0 +1,12 @@
+package org.fascinate.encody;
+
+/**
+ *
+ * @author d.schlobach
+ */
+public class L_nico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
